30e3de1edba12cb1ebc091981700ed49649b3d7e
Ethan Chen <intervigil@gmail.com>
2015-06-29 13:38:04 -0700

    msm8916: Migrate root tasks to their own cgroup
    * Migrate after boot completion
    
    Change-Id: I80b3ba0afa6a8b98abe73f24e67939ee70c1aab1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -23,6 +23,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.coex.sh
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.migrate.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.migrate.sh
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
