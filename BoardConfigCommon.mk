0fd0a7ac9e50e4c1d748a3f04dde500c016bd1df
Ethan Chen <intervigil@gmail.com>
2015-07-14 17:25:13 -0700

    msm8916: Enable Cortex-A53 errata
    Change-Id: I4248890e9a9c97413c5e63ea6412d089ba088dcc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
+TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
@@ -49,6 +50,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_VARIANT := cortex-a53
 endif
 
