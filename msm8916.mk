6e6a1c1f69e31570c8d3c5cf56cce6a3f9d11618
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-11-08 10:04:44 +0700

    msm8916-common: Enable MIDI support
    From CAF's device common LA.BR.1.3.3_rb2.2 and bacon
    
    Change-Id: I64cb845042f12e4103303e38b9cdc530041bb959

--- msm8916.mk
+++ msm8916.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
