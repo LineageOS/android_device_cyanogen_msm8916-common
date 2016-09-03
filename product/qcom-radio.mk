73915302456d0e47553553387430b9b757b30a8b
Meninblack007 <sanyam.53jain@gmail.com>
2016-09-03 17:06:06 +0530

    msm8916-common: Enable telephony-ext
    Change-Id: Idb29066b2b64de69431fbdb0efe6d6275580829c
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,6 +8,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
