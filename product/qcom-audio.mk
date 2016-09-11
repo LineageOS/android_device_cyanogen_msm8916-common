e6a9923aa9d57ca0d4a8a25c6f93803796836b74
Hriday Sharma <hridaysharma42@gmail.com>
2016-09-11 16:09:05 +0000

    msm8916: Import CAF audio configs
    * CAF Package version: LA.BR.1.2.9-00810-8x16.0
    
    Change-Id: I40d21da583df085f0bab8554ee84e8cb5805fbb9
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -9,6 +9,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     av.streaming.offload.enable=true \
