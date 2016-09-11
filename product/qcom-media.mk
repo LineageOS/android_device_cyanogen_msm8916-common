3f837f6f96b0de63cb77c04f68863eb8720bedb9
Hriday Sharma <hridaysharma42@gmail.com>
2016-09-11 16:09:05 +0000

    msm8916: Import CAF audio configs
    * CAF Package version: LA.BR.1.2.9-00810-8x16.0
    
    Change-Id: I40d21da583df085f0bab8554ee84e8cb5805fbb9
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -18,6 +18,15 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_$(TARGET_BOARD_PLATFORM_VARIANT).xml:system/etc/media_codecs.xml
 
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=1 \
