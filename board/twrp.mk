b5b4722a4c8599268951de077baa664b42f44f1f
Louis Popi <theh2o64@gmail.com>
2017-03-11 21:09:58 +0100

    msm8916-common: Add TWRP Configuration
    Change-Id: I9bc45c1deedca7dfccd2598b3b17d5a1574026f8

---
+++ board/twrp.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_VARIANT := twrp
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_CRYPTO := true
+TW_INPUT_BLACKLIST := "lis3dh-accel\x0ahbtp_vm"
+TW_THEME := portrait_hdpi
