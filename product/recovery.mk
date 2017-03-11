c2cd8f9124aaa98288e94eed7aabc785418943e6
Louis Popi <theh2o64@gmail.com>
2017-03-11 21:09:58 +0100

    msm8916-common: Add TWRP Configuration
    Change-Id: I9bc45c1deedca7dfccd2598b3b17d5a1574026f8

--- product/recovery.mk
+++ product/recovery.mk
@@ -1,3 +1,9 @@
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_cm
+
+# TWRP
+ifeq ($(WITH_TWRP),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+endif
