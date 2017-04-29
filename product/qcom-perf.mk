1c1bf7264f4420e8dfcb0681fd97e71993973307
Ritesh Saxena <riteshsax007@gmail.com>
2017-04-29 14:09:45 +0000

    msm8939: Tweak interactive for power savings
    Change-Id: Ie504a4f6a2ccfa4a689e45dcf4d2a07e3cc47abd

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -18,7 +18,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 else
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=960000 \
-    ro.min_freq_4=800000
+    ro.min_freq_0=200000 \
+    ro.min_freq_4=200000
 endif
 endif
