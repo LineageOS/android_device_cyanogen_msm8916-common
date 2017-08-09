a4ed747f31f8517dae13b7320a37bfa4c1d09542
David Ng <dave@codeaurora.org>
2017-08-08 17:15:24 -0700

    msm8916-common: Add common config.fs AID/fs configuration file
    Common config.fs/$TARGET_FS_CONFIG_GEN AID/fs configuration
    file for all chipset inheriting from base.mk.
    
    Since legacy device-specific android_filesystem_config.h and
    config.fs do not co-exist, temporarily include config.fs only
    if legacy file is absent.  Once all legacy, device-specific
    files have been deleted, config.fs can be made unconditional.
    
    Change-Id: Ie100d0cd9db808b82d99be7139f0709390c7f3a2

---
+++ config.fs
@@ -0,0 +1,62 @@
+[AID_QTI_DIAG]
+value:2901
+
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
