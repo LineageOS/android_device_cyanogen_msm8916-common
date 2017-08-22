d3f5c4ebf370b2a05f9cf3e459334ec9fe8b8297
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    msm8916-common: Update audio for O
    * Build audio HALs
    
    Change-Id: Ie06f37bea584aa49d42ce035c259da890c460f6c

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -6,6 +6,10 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing
 
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
