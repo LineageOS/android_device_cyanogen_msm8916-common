279ab22e51d4e3a15fd75c0aec4890e6620cfde1
LuK1337 <priv.luk@gmail.com>
2017-08-28 11:59:53 +0200

    msm8916-common: gps: Add flp shim
    Change-Id: I4cf64915561c60b24a1ab85f65f73b012385ea56

--- board/libshims.mk
+++ board/libshims.mk
@@ -1,2 +1,3 @@
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so 
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib64/libflp.so|libshims_flp.so
