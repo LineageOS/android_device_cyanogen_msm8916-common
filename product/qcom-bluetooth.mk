dff0e7564845f3cae4e754d780b117b38c5a6754
Ethan Chen <intervigil@gmail.com>
2017-09-29 23:14:23 -0700

    msm8916-common: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -3,10 +3,6 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
-# Init
-PRODUCT_PACKAGES += \
-    init.qcom.bt.sh
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
