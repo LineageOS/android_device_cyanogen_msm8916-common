68c6a57bf4d95d2805410b380252268a2555be6d
LuK1337 <priv.luk@gmail.com>
2018-01-26 15:51:18 +0000

    msm8916-common: Convert audio_effects.conf to XML
    * Converted using : https://github.com/luk1337/aeffects-conf2xml
    
    Change-Id: Ida4dabb206f9c9767cc17f5d002d64bca092ed94

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
