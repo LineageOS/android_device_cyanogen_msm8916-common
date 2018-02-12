d64ede80b2e0d951798bd514264a38ba4b6bb4a4
Michael Bestas <mkbestas@lineageos.org>
2018-02-12 21:55:09 +0200

    msm8916: Add loggy
    Change-Id: I22c6877e48fffa41b160fd26f74ab11cb5269d66

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -60,3 +60,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# testing for logs
+include $(CLEAR_VARS)
+LOCAL_MODULE           := loggy.sh
+LOCAL_MODULE_TAGS      := optional eng
+LOCAL_MODULE_CLASS     := ETC
+LOCAL_SRC_FILES        := etc/loggy.sh
+LOCAL_MODULE_PATH      := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
