985e276296ea3555c6a476ff397e3f7949abb186
Michael Bestas <mkbestas@lineageos.org>
2018-03-17 15:37:29 +0200

    msm8916-common: Remove fragments
    Change-Id: I0c4081efb02ec409edd6856c52ab3b7766486f03

--- board/kernel.mk
+++
@@ -1,7 +0,0 @@
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
-
-TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
--- product/display.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.use_buffer_age=false
