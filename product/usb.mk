1c1c7e29bc6b50e9d1a267106f4453b6f289d64a
Michael Bestas <mkbestas@lineageos.org>
2018-03-17 15:37:29 +0200

    msm8916-common: Remove fragments
    Change-Id: I0c4081efb02ec409edd6856c52ab3b7766486f03

--- product/usb.mk
+++
@@ -1,7 +0,0 @@
-# Init scripts
-PRODUCT_PACKAGES += \
-    init.qcom.usb.rc
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/vendor/etc/permissions/android.hardware.usb.accessory.xml
