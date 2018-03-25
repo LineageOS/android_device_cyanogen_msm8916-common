9e8f5c47884170e34f8090c07a821cfa2813baec
dianlujitao <dianlujitao@lineageos.org>
2018-03-25 17:16:17 +0800

    msm8916-common: Switch to binderized QTI power HAL
    Change-Id: I82e8386c2f3f02f408f5ac374e6f1c486b94bb29

--- msm8916.mk
+++ msm8916.mk
@@ -161,11 +161,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    power.msm8916
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
