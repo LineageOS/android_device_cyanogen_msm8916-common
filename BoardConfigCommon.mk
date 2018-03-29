7393571e3857981481b4a3b66c2c8c471d6346f7
Elektroschmock <elektroschmock78@googlemail.com>
2018-03-29 21:04:57 +0200

    msm8916-common: Clean up kernel cmdline
    * msm_rtb, depends on CONFIG_MSM_RTB which is not set for victara
    
    Change-Id: Ie78609a9df945aab556b2ab363469f882cc189bb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,7 +96,7 @@ SMALLER_FONT_FOOTPRINT := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET := 0x02000000
