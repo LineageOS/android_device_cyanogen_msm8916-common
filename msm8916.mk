662bf2d10b9dff872e5fd3afba8f7ed5170d9c39
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 22:45:28 +0300

    msm8916-common: Build with BOARD_VNDK_VERSION := current
    Change-Id: I4f142432924dc7b396094593e454a5162b09948e

--- msm8916.mk
+++ msm8916.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     libwcnss_qmi \
