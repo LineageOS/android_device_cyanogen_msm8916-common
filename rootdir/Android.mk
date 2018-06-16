0a7f4377720a0c3a7ab4ae775b4392fcc151fb6c
Michael Bestas <mkbestas@lineageos.org>
2018-06-16 15:23:55 +0300

    msm8916: Create bootdevice symlink for recovery as well
    Change-Id: I7b690696758a9b9c09b49c864217818748d43013

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,10 +54,18 @@ include $(BUILD_PREBUILT)
 
 # Copy the power config for recovery too
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE       := init.recovery.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power_$(TARGET_BOARD_PLATFORM_VARIANT).rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
