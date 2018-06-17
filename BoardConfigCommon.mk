927363d6d36be2cca2d250a7fc22d5390e4922bf
Michael Bestas <mkbestas@lineageos.org>
2018-06-17 23:06:56 +0300

    msm8916: Enable 64bit binder on 32bit devices
    * Remove the flag from 64bit as well, it's automatically enabled
    
    Change-Id: I6444651078e7cabc000d8efa48ec2eaa3d68e922

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,14 +36,14 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_USES_64_BIT_BINDER := true
 else
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
 endif
 
 # ANT+
