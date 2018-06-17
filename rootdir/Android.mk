565123e7fd169d76bf9327334bc23062c3e28a3d
Michael Bestas <mkbestas@lineageos.org>
2018-06-17 17:12:28 +0300

    msm8916: Move init.qcom.bt.sh to vendor
    Change-Id: I6b9c508e3c2b3573fba0a5bdf9281be8947fffc0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,6 +7,7 @@ LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
