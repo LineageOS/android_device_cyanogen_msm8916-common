41c65d482d6e4f8add3c26033001d605ad14c767
Michael Bestas <mkbestas@lineageos.org>
2019-01-24 19:50:35 +0200

    msm8916-common: Build LiveDisplay legacymm HAL
    * Enables picture adjustment features
    
    Change-Id: I11a09b000dd3e473a48c695f0d344bc603a08082

--- msm8916.mk
+++ msm8916.mk
@@ -126,6 +126,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-legacymm
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
