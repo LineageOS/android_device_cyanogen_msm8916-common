13a4cb42827ed3316ee1905be2c611845d169035
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 15:35:46 +0200

    msm8916: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- msm8916.mk
+++ msm8916.mk
@@ -216,4 +216,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
