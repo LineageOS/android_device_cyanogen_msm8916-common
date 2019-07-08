dc6a68feff065697ca4e0395838219b88929090f
Amit Pundir <amit.pundir@linaro.org>
2019-07-08 22:17:09 +0530

    msm8916-common: Add gatekeeper software HIDL service.
    Use the default software implementation of gatekeeper.
    
    Change-Id: Id696752ad78047155cad6a5dafe7ca1b4fe86345
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    (cherry picked from commit a7514b20dd00cf303e957904a3f2c14b191fc4b5)

--- msm8916.mk
+++ msm8916.mk
@@ -148,6 +148,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-legacymm \
