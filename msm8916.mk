3018e67f807af28f9324b2384eeb052cf2fcb4f9
Nicholas Lim <nicknitewolf@lineageos.org>
2019-07-26 20:46:48 +0800

    msm8916-common: Add qcom privapp permissions
     * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    
    Change-Id: Ia90d0bf3a9c2163f2bbd9202384d38f05b442e4f

--- msm8916.mk
+++ msm8916.mk
@@ -163,6 +163,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
