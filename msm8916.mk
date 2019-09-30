c445e0d65dd90a9a32efbc884ff5b8ecac604ad2
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    msm8916-common: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    [arco] Also clean mediacodec.policy
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- msm8916.mk
+++ msm8916.mk
@@ -200,8 +200,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # System Properties
 $(call inherit-product, device/cyanogen/msm8916-common/system_prop.mk)
