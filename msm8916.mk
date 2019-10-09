25120707cff22ef46519537243cd38c20cd31513
sb6596 <shubhamprince111@gmail.com>
2019-10-09 12:07:00 +0000

    msm8916-common: Switch to cryptfs_hw hidl service
    Change-Id: Ie924f67f90d64609af3738f20848f08c5c26b781

--- msm8916.mk
+++ msm8916.mk
@@ -87,6 +87,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
