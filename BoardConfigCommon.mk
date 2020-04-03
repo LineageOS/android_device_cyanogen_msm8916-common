ea6c3f179c8a58ffa87da5f84ec0be96977f96c7
115ek <e.blosz@hotmail.de>
2020-04-03 19:12:03 +0200

    msm8916-common: remove TARGET_KEYMASTER_WAIT_FOR_QSEE
    Change-Id: Ib64991c815a9551dba182ad8906f06e5f3dc9317

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,6 @@ TARGET_USES_ION := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Exclude serif fonts for saving system.img size.
 EXCLUDE_SERIF_FONTS := true
