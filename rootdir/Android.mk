6113146bc98867eb23b85c2777fce3afc62ef31d
Nicholas Lim <nicknitewolf@lineageos.org>
2020-05-07 01:32:19 +0800

    msm8916-common: Drop CAF Adaptive LMK script
    * In favor of Low Memory Killer Daemon (lmkd).
    * Adaptive LMK was also dropped quite sometime ago
      making it useless to keep this script around.
    
    Change-Id: I3f59ab7f3d67f79927119f9c0a99f92cc50b743d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,14 +10,6 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.mem.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.mem.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 # Init scripts
 
 include $(CLEAR_VARS)
