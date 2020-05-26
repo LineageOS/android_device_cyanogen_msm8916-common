6ecb2a919ffcbf91eff828cbd96dc92a86a07cea
Wang Han <416810799@qq.com>
2020-05-26 20:58:17 +0800

    msm8916-common: Switch to TimeKeep
     * SE policies are imported and modified from
       https://github.com/sonyxperiadev/device-sony-sepolicy.
       Modifications are needed because qcom legacy policy
       contains conflicting labels and rules.
    
    Change-Id: Id04a824dea69976f6fc9d48bef77859cc82971ed

--- msm8916.mk
+++ msm8916.mk
@@ -217,6 +217,11 @@ else ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 $(call inherit-product, device/cyanogen/msm8916-common/system_prop_msm8916.mk)
 endif
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
