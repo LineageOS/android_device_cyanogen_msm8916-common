198dabb4985679957e76908ad920d33823bf53a4
Bruno Martins <bgcngm@gmail.com>
2020-09-29 10:45:23 +0100

    msm8916-common: Build health HAL as per R requirements
    The devices fail to boot otherwise.
    
    Change-Id: Ia245ace574e3079d47287cd0e5ec908771851035

--- msm8916.mk
+++ msm8916.mk
@@ -108,6 +108,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
+
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
