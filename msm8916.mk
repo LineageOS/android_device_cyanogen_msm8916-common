4136597a28692c910c34938cd28b823bcbc883df
LuK1337 <priv.luk@gmail.com>
2020-10-01 11:52:48 +0100

    msm8916-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    Signed-off-by: RahifM <faizel326@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -118,6 +118,12 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
@@ -132,6 +138,7 @@ PRODUCT_PACKAGES += \
 
 # IRQ balance
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 endif
